482187a869aad7be6bdaf2b9e15520ba3c445150
Derrick Song <96987446+SongDerrick@users.noreply.github.com>
2023-07-24 12:18:54 +0900

    Initial Boostrap

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('landing');
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login');
 });
 
 module.exports = router;
