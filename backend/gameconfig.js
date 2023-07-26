bfd4523b848100404d60596c016c5331396cd859
박연진 <2020002960@hanyang.ac.kr>
2023-07-26 23:07:59 +0900

    fix details

--- backend/gameconfig.js
+++ backend/gameconfig.js
@@ -132,22 +132,7 @@ class Game {
   }
 
   shuffle(array) {
-    let counter = array.length;
-
-    // While there are elements in the array
-    while (counter > 0) {
-      // Pick a random index
-      let index = Math.floor(Math.random() * counter);
-
-      // Decrease counter by 1
-      counter--;
-
-      // And swap the last element with it
-      let temp = array[counter];
-      array[counter] = array[index];
-      array[index] = temp;
-    }
-
+    array.sort(() => Math.random() - 0.5);
     return array;
   }
 
